dfc7854c74e8ae4a91e98eb9f0f5b4c058d0b532
https://github.com/svkzdenosvk <zdenko.snurik@gmail.com>
2026-03-24 14:59:55 +0100

    applying shells

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -46,14 +46,7 @@ export const routes: Routes = [
         loadComponent: () =>
           import('./features/city-picker/city-picker')
             .then(m => m.CityPickerComponent)
-      }, 
-       {
-         path: 'heat-index',
-         loadComponent: () =>
-         import('./features/heat-index-calculator/heat-index-calculator')
-         .then(m => m.HeatIndexCalculatorComponent)
-       }
-
+      }
     ]
   },
   {
@@ -61,26 +54,27 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./shared/shells_layouts/app-shell/app-shell')
         .then(m => m.AppShell),
-    canActivate: [cityGuard],
     children: [
       {
         path: 'weather',
+        canActivate: [cityGuard],
         loadComponent: () =>
           import('./features/weather-table/weather-table')
             .then(m => m.WeatherTableComponent)
       },
       {
         path: 'chart',
+        canActivate: [cityGuard],
         loadComponent: () =>
           import('./features/temperature-chart/temperature-chart')
             .then(m => m.TemperatureChartComponent)
-       }]
-      // {
-      //   path: 'heat-index',
-      //   loadComponent: () =>
-      //     import('./features/heat-index-calculator/heat-index-calculator')
-      //       .then(m => m.HeatIndexCalculatorComponent)
-      // }
-    // ]
+      },
+      {
+        path: 'heat-index',
+        loadComponent: () =>
+          import('./features/heat-index-calculator/heat-index-calculator')
+            .then(m => m.HeatIndexCalculatorComponent)
+      }
+    ]
   }
 ];
