24d221490f32aa442b333f7c4c925ade8a6d54af
https://github.com/svkzdenosvk <zdenko.snurik@gmail.com>
2026-04-29 02:56:48 +0200

    change apiUrl for production deploy to work it correctly

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
-// src/environments/environment.production.ts  
+// src/environments/environment.production.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://weather-backend-ub7j.onrender.com/'
+  apiUrl: 'https://weather-backend-ub7j.onrender.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  // apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://weather-backend-ub7j.onrender.com',
 };
