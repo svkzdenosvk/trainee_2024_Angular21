9372feb66b7b6edf9b84481e786701c10acfd122
Zdeno <zdenko.snurik@gmail.com>
2026-05-19 02:32:44 +0200

    lang buttons moved under login info and fixing mobile responsive design for link in nav

--- src/app/shared/components/app-nav-auth/app-nav-auth.ts
+++ src/app/shared/components/app-nav-auth/app-nav-auth.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
 import { AuthService } from '../../../core/services/auth.service';
 import { Router } from '@angular/router';
+import { AppLangSwitcher } from '../_app-lang-switcher/app-lang-switcher';
 
 @Component({
   selector: 'app-nav-auth',
-  imports: [TranslocoModule],
+  imports: [TranslocoModule, AppLangSwitcher],
   templateUrl: './app-nav-auth.html',
   styleUrl: './app-nav-auth.scss',
 })
