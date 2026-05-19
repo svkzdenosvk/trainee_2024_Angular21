275a0aeabf56eca4e62df77c455500f16a49648d
Zdeno <zdenko.snurik@gmail.com>
2026-05-19 12:09:34 +0200

    adding hamburger icon

--- src/app/shared/components/app-public-nav/app-public-nav.ts
+++ src/app/shared/components/app-public-nav/app-public-nav.ts
@@ -1,16 +1,26 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslocoModule } from '@jsverse/transloco';
 import { AuthService } from '../../../core/services/auth.service';
-import { AppLangSwitcher } from '../_app-lang-switcher/app-lang-switcher';
+// import { AppLangSwitcher } from '../_app-lang-switcher/app-lang-switcher';
 
 @Component({
   selector: 'app-public-nav',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, TranslocoModule, AppLangSwitcher],
+  imports: [RouterLink, RouterLinkActive, TranslocoModule,/* AppLangSwitcher*/],
   templateUrl: './app-public-nav.html',
   styleUrl: '../../scss/_shells.scss',
 })
 export class AppPublicNav {
   protected readonly authService = inject(AuthService);
+
+  isOpen = signal(false);
+
+  toggleMenu(): void {
+    this.isOpen.update(value => !value);
+  }
+
+  closeMenu(): void {
+    this.isOpen.set(false);
+  }
 }
