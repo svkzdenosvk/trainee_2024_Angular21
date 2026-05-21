91da2315cf7411c20b6c45e032733b09f6c6a404
Zdeno <zdenko.snurik@gmail.com>
2026-05-21 03:42:09 +0200

    try to fix set cookies again, again

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.production.ts
 export const environment = {
   production: true,
-  // apiUrl: 'https://weather-backend-ub7j.onrender.com',
-  apiUrl: '/api'
+  apiUrl: 'https://weather-backend-ub7j.onrender.com',
+  // apiUrl: '/api'
 };
