223982a88e61a83d0a511e651856ef0a97a250a0
Zdeno <zdenko.snurik@gmail.com>
2026-05-29 08:07:01 +0200

    changed backend production to new docker url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,8 @@
 // src/environments/environment.production.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://weather-backend-ub7j.onrender.com',
+  ///////////// apiUrl: 'https://weather-backend-ub7j.onrender.com',
+  apiUrl: 'https://weather-backend-docker.onrender.com',
+
   // apiUrl: '/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
-  // apiUrl: 'https://weather-backend-ub7j.onrender.com',
+  /////////// apiUrl: 'https://weather-backend-ub7j.onrender.com',
+  // apiUrl: 'http://localhost:3000',
+  apiUrl:'https://weather-backend-docker.onrender.com'
 };
 
