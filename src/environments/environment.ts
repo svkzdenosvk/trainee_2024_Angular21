acf68a8612c05444af0d4bed435445dbecf73d92
Zdeno <zdenko.snurik@gmail.com>
2026-06-03 22:39:02 +0200

    change production backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@
 export const environment = {
   production: false,
   /////////// apiUrl: 'https://weather-backend-ub7j.onrender.com',
-  apiUrl: 'http://localhost:3000',
-  // apiUrl: 'https://weather-backend-docker.onrender.com',
+  ///////////////////////////////////////////////////////////////////////
+  // apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://weather-backend-docker.onrender.com',
 };
